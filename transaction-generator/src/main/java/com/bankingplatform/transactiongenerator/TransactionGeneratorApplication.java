7ff4e297ba7833fc0b5a72965aa0c16e97d570a7
S1nRTax <badreddine.asaad4@gmail.com>
2025-09-12 19:23:49 +0100

    Defined main application class + modified application properties

--- transaction-generator/src/main/java/com/bankingplatform/transactiongenerator/TransactionGeneratorApplication.java
+++ transaction-generator/src/main/java/com/bankingplatform/transactiongenerator/TransactionGeneratorApplication.java
@@ -2,8 +2,12 @@ package com.bankingplatform.transactiongenerator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableConfigurationProperties
 public class TransactionGeneratorApplication {
 
 	public static void main(String[] args) {
