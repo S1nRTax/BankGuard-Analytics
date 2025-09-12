fcf16e265c82e74117e250574f58fe165e28a237
S1nRTax <badreddine.asaad4@gmail.com>
2025-09-12 18:17:11 +0100

    Implement transaction generator model

---
+++ transaction-generator/src/main/java/com/bankingplatform/transactiongenerator/model/TransactionStatus.java
@@ -0,0 +1,9 @@
+package com.bankingplatform.transactiongenerator.model;
+
+public enum TransactionStatus {
+    PENDING,
+    COMPLETED,
+    FAILED,
+    CANCELLED,
+    PROCESSING
+}
---
+++ transaction-generator/src/main/java/com/bankingplatform/transactiongenerator/model/TransactionType.java
@@ -0,0 +1,11 @@
+package com.bankingplatform.transactiongenerator.model;
+
+public enum TransactionType {
+    PAYMENT,
+    TRANSFER,
+    WITHDRAWAL,
+    DEPOSIT,
+    REFUND,
+    SUBSCRIPTION,
+    INVESTMENT
+}
