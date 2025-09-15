b5dddd96ca01eaeae92880e3c27242afc40b84bf
S1nRTax <badreddine.asaad4@gmail.com>
2025-09-15 18:01:20 +0100

    implement main entry for stream-processor && sql script for db init

--- stream-processor/src/main/java/com/bankingplatform/streamprocessor/StreamProcessorApplication.java
+++ stream-processor/src/main/java/com/bankingplatform/streamprocessor/StreamProcessorApplication.java
@@ -1,13 +1,19 @@
+// StreamProcessorApplication.java
 package com.bankingplatform.streamprocessor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableKafkaStreams
+@EnableScheduling
+@EnableTransactionManagement
 public class StreamProcessorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StreamProcessorApplication.class, args);
 	}
-
 }
